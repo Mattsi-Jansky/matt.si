eeb76405d4819740aade6a9391dc18d34a80d946
Mattsi <mattsi.jansky@codurance.com>
2019-11-02 09:53:46 +0000

    Refactor gatsby-node.js for readability

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,12 +2,13 @@ const path = require(`path`)
 const { createFilePath } = require(`gatsby-source-filesystem`)
 const generatePathToTag = require('./src/shared/pathToTagGenerator.js')
 
+const postsPerPage = 10
+const blogPostTemplate = path.resolve(`./src/templates/blog-post.js`)
+const blogListTemplate = path.resolve("./src/templates/blog-list.js")
+const tagTemplate = path.resolve(`src/templates/tag.js`)
+
 exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions
-
-  const blogPostTemplate = path.resolve(`./src/templates/blog-post.js`)
-  const tagTemplate = path.resolve(`src/templates/tag.js`);
-
   return graphql(
     `
       {
@@ -34,58 +35,71 @@ exports.createPages = ({ graphql, actions }) => {
       throw result.errors
     }
 
-    // Create blog posts pages.
     const posts = result.data.allMarkdownRemark.edges
+    CreateBlogPostPages(posts, createPage)
+    CreateBlogPostListPages(posts, createPage)
 
-    posts.forEach((post, index) => {
-      const previous = index === posts.length - 1 ? null : posts[index + 1].node
-      const next = index === 0 ? null : posts[index - 1].node
+    let tags = CompileTags(posts)
+    CreateTagsPages(tags, createPage)
+  })
+}
 
-      createPage({
-        path: post.node.fields.slug,
-        component: blogPostTemplate,
-        context: {
-          slug: post.node.fields.slug,
-          previous,
-          next,
-        },
-      })
+function CreateBlogPostPages(posts, createPage) {
+  posts.forEach((post, index) => {
+    const previous = index === posts.length - 1 ? null : posts[index + 1].node
+    const next = index === 0 ? null : posts[index - 1].node
+
+    createPage({
+      path: post.node.fields.slug,
+      component: blogPostTemplate,
+      context: {
+        slug: post.node.fields.slug,
+        previous,
+        next,
+      },
     })
+  })
+}
 
-    // Create blog post list pages
-    const postsPerPage = 10
-    const numPages = Math.ceil(posts.length / postsPerPage)
+function CreateBlogPostListPages(posts, createPage) {
+  const numPages = Math.ceil(posts.length / postsPerPage)
 
-    Array.from({ length: numPages }).forEach((_, i) => {
-      createPage({
-        path: i === 0 ? `/` : `/${i + 1}`,
-        component: path.resolve("./src/templates/blog-list.js"),
-        context: {
-          limit: postsPerPage,
-          skip: i * postsPerPage,
-          numPages,
-          currentPage: i + 1,
-        },
-      })
+  Array.from({ length: numPages }).forEach((_, i) => {
+    createPage({
+      path: i === 0 ? `/` : `/${i + 1}`,
+      component: blogListTemplate,
+      context: {
+        limit: postsPerPage,
+        skip: i * postsPerPage,
+        numPages,
+        currentPage: i + 1,
+      },
     })
+  })
+}
 
-    let tags = new Set()
-    posts.forEach(({ node }) => {
-      if(node.frontmatter.tags) {
-        node.frontmatter.tags.forEach((tag) => 
-          tags.add(tag)
-        )
+function CompileTags(posts,) {
+  let tags = new Set()
+
+  posts.forEach(({ node }) => {
+    if(node.frontmatter.tags) {
+      node.frontmatter.tags.forEach((tag) => 
+        tags.add(tag)
+      )
+    }
+  })
+
+  return tags
+}
+
+function CreateTagsPages(tags, createPage) {
+  tags.forEach( tag => {
+    createPage({
+      path: generatePathToTag(tag),
+      component: tagTemplate,
+      context: {
+        tag
       }
-    })
-    
-    tags.forEach( tag => {
-      createPage({
-        path: generatePathToTag(tag),
-        component: tagTemplate,
-        context: {
-          tag
-        }
-      })
     })
   })
 }
