bedfe25cd597a3cb951df12082fa1ec13f836085
Mattsi <mattsi.jansky@codurance.com>
2022-04-21 21:12:15 +0100

    Fix numerous problems, dependency conflicts
    It's late I'm tired

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,12 +43,21 @@ module.exports = {
         ],
       },
     },
+    `gatsby-plugin-image`,
     `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: `gatsby-plugin-sharp`,
       options: {
-        trackingId: `UA-54448080-1`,
+        // Defaults used for gatsbyImageData and StaticImage
+        defaults: {},
+        // Set to false to allow builds to continue on image errors
+        failOnError: true,
+        // deprecated options and their defaults:
+        base64Width: 20,
+        forceBase64Format: `jpg`,
+        useMozJpeg: process.env.GATSBY_JPEG_ENCODER === `MOZJPEG`,
+        stripMetadata: true,
+        defaultQuality: 50,
       },
     },
     {
