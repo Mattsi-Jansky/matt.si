a5052a47bb625eef9c588e4c951424c0fd4d2951
Mattsi <mattsi.jansky@codurance.com>
2023-06-26 12:54:32 +0100

    Reblog Codurance angular migration post

--- src/components/figure.js
+++ src/components/figure.js
@@ -2,28 +2,28 @@ import React from 'react'
 import { graphql, StaticQuery } from 'gatsby'
 import Img from 'gatsby-image'
 
-const InnerFigure = ({ data, children, width }) => {
+const InnerFigure = ({ data, children, width, alt }) => {
   if(!width) width = "100%"
   return (
     <div id="content-figure" style={{width: width}}>
       <figure>
         <a href={data.src} target="_blank">
-          <Img fluid={data}/>
+          <Img alt={alt} fluid={data}/>
         </a>
         {children && <figcaption>{children}</figcaption>}
       </figure>
     </div>)
 }
 
-const renderFigureWithQueryResults = (src, children, width) => data => {
+const renderFigureWithQueryResults = (src, children, width, alt) => data => {
   var queryResult = data.figureQuery.edges.find(
     element => element.node.fluid.src.split('/').pop() === src
   )
   if (!queryResult) throw new Error("Figure Comoponent: Can't find image")
-  return <InnerFigure children={children} data={queryResult.node.fluid} width={width} />
+  return <InnerFigure children={children} data={queryResult.node.fluid} width={width} alt={alt} />
 }
 
-export default ({ children, src, width }) => (
+export default ({ children, src, width, alt }) => (
   <StaticQuery
     query={graphql`
       query FigureQuery {
@@ -38,7 +38,7 @@ export default ({ children, src, width }) => (
         }
       }
     `}
-    render={renderFigureWithQueryResults(src, children, width)}
+    render={renderFigureWithQueryResults(src, children, width, alt)}
   />
 )
 export { InnerFigure }
