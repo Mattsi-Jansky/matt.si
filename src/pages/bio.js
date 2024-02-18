5145d33a205dd32d58f26eabfdb081e3caefc78e
Mattsi <mattsi.jansky@codurance.com>
2024-02-18 17:19:42 +0000

    A bit more bio

--- src/pages/bio.js
+++ src/pages/bio.js
@@ -15,7 +15,8 @@ const Bio = () => {
             <p>In 1936 Alan Turing published "On Computable Numbers", proving that it was possible to create a computer. This has made many people very angry and has been widely regarded as a bad move.</p>
           </blockquote>
           <p>I'm Mattsi, a software engineer / consultant / craftsman / tell-computers-what-to-do-guy working remotely at Codurance.
-            I am a polygot and use a wide variety of tech in my day work. In my personal projects I've mostly been sticking to Rust in recent years.</p>
+            I am a polygot and use a wide variety of tech in my day work. In my personal projects I've mostly been sticking to Rust in recent years.
+            Sometimes I speak at events or conferences, and write blog posts.</p>
           <p>
             You've found yourself on my blog. I mostly write about software development and whatever programming languages, side projects or conferences/events I'm interested in lately. You can find more of my ramblings and code on Mastodon, GitHub, etc linked at the bottom of the sidebar, and an RSS feed for the blog.
           </p>
