2c7a78bd93f955f46b95c63583ff7e6e2f6d02fd
Mattsi <mattsi@jansky.dev>
2026-04-01 11:48:52 +0100

    Serve social icons, no CDN

--- src/components/seo.js
+++ src/components/seo.js
@@ -31,7 +31,6 @@ function SEO({ description, title, canonicalLink, children }) {
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={metaDescription} />
       {canonicalLink && <link rel="canonical" href={canonicalLink} />}
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
       {children}
     </>
   )
