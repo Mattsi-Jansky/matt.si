4495f94a891dcf1714bef8d1411ef574005212cc
Mattsi <mattsi@jansky.dev>
2026-04-01 11:43:25 +0100

    Do not use CDN for fonts

--- src/components/seo.js
+++ src/components/seo.js
@@ -31,10 +31,6 @@ function SEO({ description, title, canonicalLink, children }) {
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={metaDescription} />
       {canonicalLink && <link rel="canonical" href={canonicalLink} />}
-      <link
-        href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,300;0,400;0,500;0,600;1,300;1,400&family=Quicksand:wght@400;500;600;700&display=swap"
-        rel="stylesheet"
-      />
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
       {children}
     </>
